eea6283916ef47a6e0216c4a1cfd9d5cf323223f
Scott Schraeder <CosignCosine@users.noreply.github.com>
2018-01-15 13:52:01 -0500

    noodle

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ var peopleIHate = ["nobody publicly"];
 
 // don't change this
 var peopleRegex = new RegExp(peopleIHate.join("|"), "gim");
-$(".author-nickname").each(function(el) {
+$(".author-nickname").each(function() {
     if ($(this).text().match(peopleRegex)) {
         $(this).parent().parent().parent().remove();
+    }
+})
+$("a").each(function(){
+    if($(this).attr('class').match(/author/gim) && $(this).text().match(peopleRegex)){
+        $(this).parent().parent().remove();
     }
 })
