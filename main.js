a874c5268f62301e9a6c1b2c4c6aaf9f3962ad41
Scott Schraeder <CosignCosine@users.noreply.github.com>
2018-01-15 14:01:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 // change this
-var peopleIHate = ["nobody publicly"];
+var peopleIHate = ["nobody currently"];
 
 // don't change this
 var peopleRegex = new RegExp(peopleIHate.join("|"), "gim");
+function deletionTest(el, depth){
+    if ($(el) && $(el).text().match(peopleRegex)) {
+        var cl = $(el);
+        while(depth > 0){
+            cl = cl.parent();
+            depth--;
+        }
+        cl.remove();
+    }
+}
 $(".author-nickname").each(function() {
-    if ($(this).text().match(peopleRegex)) {
-        $(this).parent().parent().parent().remove();
-    }
+        deletionTest($(this), 3);
 })
-$("a").each(function(){
-    if($(this).attr('class').match(/author/gim) && $(this).text().match(peopleRegex)){
-        $(this).parent().parent().remove();
-    }
-})
+if(location.href.includes('computing')){
+    $("a").each(function(){
+        if($(this).attr('class').match(/author/gim)){
+            deletionTest(this, 2);
+        }
+    })
+}
+if(location.href.includes('profile')){
+    $(".nickname").each(function(){
+        deletionTest(this, 11);
+    })
+}
