f84c6496987f197c919c47966c90d73ad08d00a2
Scott Schraeder <CosignCosine@users.noreply.github.com>
2018-01-15 14:19:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 // change this
-var peopleIHate = ["nobody currently"];
+var peopleIHate = localStorage.getItem("cosigncosine-block") ? JSON.parse(localStorage.getItem("cosigncosine-block")) : [];
 
 // don't change this
 var peopleRegex = new RegExp(peopleIHate.join("|"), "gim");
 function deletionTest(el, depth){
-    if ($(el) && $(el).text().match(peopleRegex)) {
-        var cl = $(el);
-        while(depth > 0){
-            cl = cl.parent();
-            depth--;
+    if(peopleIHate.length !== 0){
+        if ($(el) && $(el).text().match(peopleRegex)) {
+            var cl = $(el);
+            while(depth > 0){
+                cl = cl.parent();
+                depth--;
+            }
+            cl.remove();
         }
-        cl.remove();
     }
 }
 $(".author-nickname").each(function() {
         deletionTest(this, 3);
+})
+$(".discussion-meta-controls").each(function(){
+    $("<span>").html("•").addClass("discussion-meta-separator").appendTo(this);
+    $("<span>").html("<a href='javascript:void(0);' class='cosigncosine-block' title='Block this user'>Block</a>").addClass("flag-tools").appendTo(this);
 })
 if(location.href.includes('computing')){
     $("a").each(function(){
@@ -28,3 +34,13 @@ if(location.href.includes('profile')){
         deletionTest(this, 11);
     })
 }
+
+//handlers
+$('.cosigncosine-block').on('click', function(){
+    var user = $(this).parent().parent().parent().find('.discussion-meta-info').find('.author-nickname').text().replace("View profile for: ", "").trim();
+    console.log(user);
+    peopleIHate.push(user);
+    peopleRegex = new RegExp(peopleIHate.join("|"), "gim");
+    localStorage.setItem("cosigncosine-block", JSON.stringify(peopleIHate))
+    console.log(localStorage.getItem('cosigncosine-block'));
+})
